8c47efba5647695f448d631fcea93d68cf73a5b9
awbb <7593907+awbb@user.noreply.gitee.com>
2021-09-09 12:56:19 +0800

    create fire_bullet

--- game_functions.py
+++ game_functions.py
@@ -10,10 +10,13 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
   elif event.key == pygame.K_LEFT:
     ship.moving_left = True
   elif event.key == pygame.K_SPACE:
-    # 创建一颗子弹，并将其加入到编组bullets中
-    if len(bullets) < ai_settings.bullets_allowed:
-      new_bullet = Bullet(ai_settings, screen, ship)
-      bullets.add(new_bullet)
+    fire_bullet(ai_settings, screen, ship, bullets)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+  # 创建一颗子弹，并将其加入到编组bullets中
+  if len(bullets) < ai_settings.bullets_allowed:
+    new_bullet = Bullet(ai_settings, screen, ship)
+    bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
   """响应松开"""
